5ede4816d3ee1690f7982a45430c39fc5479fbce
prince <hyperqash1@gmail.com>
2026-02-06 11:32:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const hour = new Date().getHours();
+let message = "Hello!";
+
+if (hour < 12) {
+  message = "Good morning!";
+} else if (hour < 18) {
+  message = "Good afternoon!";
+} else {
+  message = "Good evening!";
+}
+
+document.getElementById("greeting").innerText =
+  message + " Welcome to Nairobi Garbage Tracker!";
